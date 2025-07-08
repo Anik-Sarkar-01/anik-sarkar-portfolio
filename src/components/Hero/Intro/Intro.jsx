f63841516ccf0a9ac4e5f2e9b319b100d44f5910
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-07-09 00:18:24 +0600

    box background removed

--- src/components/Hero/Intro/Intro.jsx
+++ src/components/Hero/Intro/Intro.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { FiDownload } from "react-icons/fi";
-import { BoxesCore } from '../../ui/background-boxs';
 
 
 const Intro = () => {
     return (
-        <div className='p-8 bg-[#0D1321] text-white rounded-xl relative overflow-hidden'>
-            <BoxesCore></BoxesCore>
-            <div className=' space-y-5 relative'>
-                <p>Hi there!</p>
-                <h1 className='text-xl md:text-3xl lg:text-4xl'>I'm <span className='text-[#00ff99]'>Anik Sarkar</span>, a Web Developer crafting user-centric web interfaces.</h1>
-                <button className='flex items-center gap-1 bg-[#00ff99] p-2 text-black rounded-lg'>Download Resume <FiDownload /> </button>
-            </div>
+        <div className='p-8 bg-[#0D1321] space-y-5 text-white rounded-xl'>
+            <p>Hi there!</p>
+            <h1 className='text-xl md:text-3xl lg:text-4xl'>I'm <span className='text-[#00ff99]'>Anik Sarkar</span>, a Web Developer crafting user-centric web interfaces.</h1>
+            <button className='flex items-center gap-1 bg-[#00ff99] p-2 text-black rounded-lg'>Download Resume <FiDownload /> </button>
         </div>
     );
 };
--- src/components/Hero/SideBar/SideBar.jsx
+++ src/components/Hero/SideBar/SideBar.jsx
@@ -3,13 +3,11 @@ import profilePic from "../../../assets/profile.png";
 import { FaLinkedin, FaFacebook, FaGithub } from "react-icons/fa6";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-import { BoxesCore } from '../../ui/background-boxs';
 
 const SideBar = () => {
     return (
         <div className="flex justify-center p-4">
-            <div className='w-fit lg:w-[400px] h-auto bg-[#0D1321] text-white p-12 flex flex-col justify-center items-center space-y-6 rounded-xl shadow-xl relative overflow-hidden'>
-                <BoxesCore></BoxesCore>
+            <div className='w-fit lg:w-[400px] h-auto bg-[#0D1321] text-white p-12 flex flex-col justify-center items-center space-y-6 rounded-xl shadow-xl'>
                 {/* Profile Image with Animated Ring */}
                 <div className='w-60 h-60 relative flex items-center justify-center'>
                     <motion.svg
@@ -47,10 +45,8 @@ const SideBar = () => {
                 </div>
 
                 {/* Name and Bio */}
-
-                <div className='relative'> 
                     <h1 className='text-3xl font-bold text-center'>Anik Sarkar</h1>
-                    <p className='text-center text-gray-300'>A Web Enthusiast based in Bangladesh.</p>
+                    <p className='text-center text-gray-300'>A Frontend Developer based in Bangladesh.</p>
 
                     {/* Social Icons */}
                     <div className='flex items-center justify-center gap-6 text-2xl mt-2'>
@@ -81,7 +77,6 @@ const SideBar = () => {
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
